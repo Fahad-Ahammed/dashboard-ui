53e30f2528dbec91f31738eddfaf6bb76b213c8a
Fahad <fahad04598@gmal.com>
2024-02-05 14:14:47 +0530

    google auth bug fix

--- src/components/layouts/admin-layout/index.tsx
+++ src/components/layouts/admin-layout/index.tsx
@@ -1,25 +1,17 @@
 import Header from "@/components/Headers/admin";
 import { Profile } from "@/components/Headers/admin";
 import { checkAuthentication } from "@/utils/auth";
-import { useLayoutEffect, useState } from "react";
+import { useState } from "react";
 import { useRouter } from "next/router";
 import { useSession } from "next-auth/react";
 
 const Index = ({ children }: any) => {
-  const router = useRouter();
-  const [isAuthenticated, setAuthenticated] = useState(false);
-  const { status } = useSession();
 
-  useLayoutEffect(() => {
-    if (checkAuthentication() || status === "authenticated") {
-      setAuthenticated(true);
-    } else {
-      router.push("/");
-    }
-  }, [router,status]);
+  const { data: session } = useSession();
+
   return (
     <>
-      {isAuthenticated && (
+      {(checkAuthentication() || session) && (
         <div className="lg:flex max-w-[1600px] mx-auto ">
           <Header />
           <main className="flex-1">{children}</main>
