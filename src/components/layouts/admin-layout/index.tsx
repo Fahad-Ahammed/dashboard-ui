619720f944389d46a23529bf2ac610f5daff3f28
Fahad <fahad04598@gmal.com>
2024-02-05 15:32:01 +0530

    add middleware to protect routes

--- src/components/layouts/admin-layout/index.tsx
+++ src/components/layouts/admin-layout/index.tsx
@@ -1,12 +1,10 @@
 import Header from "@/components/Headers/admin";
 import { Profile } from "@/components/Headers/admin";
 import { checkAuthentication } from "@/utils/auth";
-import { useState } from "react";
-import { useRouter } from "next/router";
 import { useSession } from "next-auth/react";
 
 const Index = ({ children }: any) => {
-
+  
   const { data: session } = useSession();
 
   return (
