eb5d365508ac0986e7adb2ff7e8199dc6d8768af
Fahad <fahad04598@gmal.com>
2024-02-05 09:02:58 +0530

    fix google auth redirection

--- src/components/layouts/admin-layout/index.tsx
+++ src/components/layouts/admin-layout/index.tsx
@@ -3,17 +3,20 @@ import { Profile } from "@/components/Headers/admin";
 import { checkAuthentication } from "@/utils/auth";
 import { useLayoutEffect, useState } from "react";
 import { useRouter } from "next/router";
+import { useSession } from "next-auth/react";
 
 const Index = ({ children }: any) => {
   const router = useRouter();
   const [isAuthenticated, setAuthenticated] = useState(false);
+  const { status } = useSession();
+
   useLayoutEffect(() => {
-    if (!checkAuthentication()) {
-      router.push("/");
-    } else {
+    if (checkAuthentication() || status === "authenticated") {
       setAuthenticated(true);
+    } else {
+      router.push("/");
     }
-  }, [router]);
+  }, [router,status]);
   return (
     <>
       {isAuthenticated && (
